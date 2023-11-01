c91b33a37c2a4c102ee1929c45eb9a73cd305aa9
Murat <85807376+yeniref@users.noreply.github.com>
2023-11-01 15:36:22 +0300

    Update gizle-goster-eklentisi.php

--- gizle-goster-eklentisi.php
+++ gizle-goster-eklentisi.php
@@ -9,58 +9,64 @@ License: GPL2
 */
 
 
-function eklenti_gizle_goster_metabox() {
-    add_meta_box('gizle-goster-metabox', 'Yazıyı Gizle veya Göster', 'gizle_goster_metabox_icerik', 'post', 'side', 'default');
-}
-add_action('add_meta_boxes', 'eklenti_gizle_goster_metabox');
+class Eklenti_Gizle_Goster {
 
-function gizle_goster_metabox_icerik($post) {
-    $gizli = get_post_meta($post->ID, '_yazi_gizli', true);
-    ?>
-    <label for="yazi_gizli">Bu yazıyı gizle:
-        <input type="checkbox" id="yazi_gizli" name="yazi_gizli" <?php echo $gizli ? 'checked' : ''; ?> />
-    </label>
-    <?php
-}
-
-// Yazıyı gizle veya göster seçeneğini kaydetmek
-function eklenti_gizle_goster_kaydet($post_id) {
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-    if (isset($_POST['yazi_gizli'])) {
-        update_post_meta($post_id, '_yazi_gizli', 1);
-    } else {
-        delete_post_meta($post_id, '_yazi_gizli');
+    public function __construct() {
+        add_action('add_meta_boxes', array($this, 'eklenti_gizle_goster_metabox'));
+        add_action('save_post', array($this, 'eklenti_gizle_goster_kaydet'));
+        add_filter('the_content', array($this, 'gizli_yazilari_duzenle'));
+        add_filter('get_the_content', array($this, 'gizli_yazi_getir'));
     }
-}
-add_action('save_post', 'eklenti_gizle_goster_kaydet');
 
-// Yazıları gizlemek veya göstermek
-function gizli_yazilari_duzenle($content) {
-    if (is_single()) {
-        if (!is_user_logged_in()) { // Üye olmayan kullanıcıları kontrol et
-            $content = '<h3>Üzgünüz, bu yazı yalnızca üyelere özeldir. Lütfen giriş yapın veya üye olun.</h3>';
+    public function eklenti_gizle_goster_metabox() {
+        add_meta_box('gizle-goster-metabox', 'Yazıyı Gizle veya Göster', array($this, 'gizle_goster_metabox_icerik'), 'post', 'side', 'default');
+    }
+
+    public function gizle_goster_metabox_icerik($post) {
+        $gizli = get_post_meta($post->ID, '_yazi_gizli', true);
+        ?>
+        <label for="yazi_gizli">Bu yazıyı gizle:
+            <input type="checkbox" id="yazi_gizli" name="yazi_gizli" <?php echo $gizli ? 'checked' : ''; ?> />
+        </label>
+        <?php
+    }
+
+    public function eklenti_gizle_goster_kaydet($post_id) {
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+        if (isset($_POST['yazi_gizli'])) {
+            update_post_meta($post_id, '_yazi_gizli', 1);
         } else {
-            $gizli = get_post_meta(get_the_ID(), '_yazi_gizli', true);
-            if ($gizli) {
-              return  $content;
-            }
+            delete_post_meta($post_id, '_yazi_gizli');
         }
     }
-    return $content;
-}
-add_filter('the_content', 'gizli_yazilari_duzenle');
 
-function gizli_yazi_getir($content) {
-    if (is_single()) {
-        if (!is_user_logged_in()) { // Üye olmayan kullanıcıları kontrol et
-            $content = '<h3>Üzgünüz, bu yazı yalnızca üyelere özeldir. Lütfen giriş yapın veya üye olun.</h3>';
-        } else {
-            $gizli = get_post_meta(get_the_ID(), '_yazi_gizli', true);
-            if ($gizli) {
-                return  $content;
+    public function gizli_yazilari_duzenle($content) {
+        if (is_single()) {
+            if (!is_user_logged_in()) { // Üye olmayan kullanıcıları kontrol et
+                $content = '<h3>Üzgünüz, bu yazı yalnızca üyelere özeldir. Lütfen giriş yapın veya üye olun.</h3>';
+            } else {
+                $gizli = get_post_meta(get_the_ID(), '_yazi_gizli', true);
+                if ($gizli) {
+                    return $content;
+                }
             }
         }
+        return $content;
     }
-    return $content;
+
+    public function gizli_yazi_getir($content) {
+        if (is_single()) {
+            if (!is_user_logged_in()) { // Üye olmayan kullanıcıları kontrol et
+                $content = '<h3>Üzgünüz, bu yazı yalnızca üyelere özeldir. Lütfen giriş yapın veya üye olun.</h3>';
+            } else {
+                $gizli = get_post_meta(get_the_ID(), '_yazi_gizli', true);
+                if ($gizli) {
+                    return $content;
+                }
+            }
+        }
+        return $content;
+    }
 }
-add_filter('get_the_content', 'gizli_yazi_getir');
+
+$eklenti_gizle_goster = new Eklenti_Gizle_Goster();
